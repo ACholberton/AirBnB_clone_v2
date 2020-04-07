14a58a70865ae506212424677d3aaf78c086593d
ACholberton <1103@holbertonschool.com>
2020-04-07 03:07:59 +0000

    fixed typos, but code still ends in nginx error

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,16 +2,16 @@
 # script that sets up your web servers for the deployment of web_static
 
 sudo apt-get -y update
-sudp apt-get -y install nginx
+sudo apt-get -y install nginx
 
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 
 echo "<html><head>Hi Im Andres </head></html>" >> /data/web_static/releases/test/index.html
 ln -fs /data/web_static/current/ /data/web_static/releases/test/
-chown -R ubuntu:ubuntu/data
+sudo chown -R ubuntu:ubuntu/data
 
 sudo sed -i "/listen 80 default_server/a location /hbnb_static {alias /data/web_static/current/;}" /etc/nginx/sites-available/default
 sudo ufw allow "Nginx HTTP"
 sudo service nginx restart
-exit(0)
+exit 0
