31d41945775d871b42b9d8648b0ec6df20ceccf8
ACholberton <1103@holbertonschool.com>
2020-04-07 03:54:21 +0000

    added sudo to all commands and fixed typos

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,9 +7,9 @@ sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 
-echo "<html><head>Hi Im Andres </head></html>" >> /data/web_static/releases/test/index.html
-ln -fs /data/web_static/current/ /data/web_static/releases/test/
-sudo chown -R ubuntu:ubuntu/data
+sudo echo "<html><head>Hi Im Andres </head></html>" >> /data/web_static/releases/test/index.html
+sudo ln -fs /data/web_static/current/ /data/web_static/releases/test/
+sudo chown -R ubuntu:ubuntu /data
 
 sudo sed -i "/listen 80 default_server/a location /hbnb_static {alias /data/web_static/current/;}" /etc/nginx/sites-available/default
 sudo ufw allow "Nginx HTTP"
