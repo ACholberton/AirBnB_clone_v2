11d31cc6c3cda2c338c25c146d06ffca3ef4fa78
ACholberton <1103@holbertonschool.com>
2020-04-07 02:55:13 +0000

    created script to set up directories and permisions

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# script that sets up your web servers for the deployment of web_static
+
+sudo apt-get -y update
+sudp apt-get -y install nginx
+
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+
+echo "<html><head>Hi Im Andres </head></html>" >> /data/web_static/releases/test/index.html
+ln -fs /data/web_static/current/ /data/web_static/releases/test/
+chown -R ubuntu:ubuntu/data
+
+sudo sed -i "/listen 80 default_server/a location /hbnb_static {alias /data/web_static/current/;}" /etc/nginx/sites-available/default
+sudo ufw allow "Nginx HTTP"
+sudo service nginx restart
+exit(0)
