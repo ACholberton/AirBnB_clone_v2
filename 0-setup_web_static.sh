3d1378d773dfe98c498f37499c0ee21d31c09f90
ACholberton <1103@holbertonschool.com>
2020-04-10 16:44:59 +0000

    removed extra /

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,10 +8,10 @@ sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 
 sudo echo "<html><head>Hi Im Andres </head></html>" >> /data/web_static/releases/test/index.html
-sudo ln -fs /data/web_static/releases/test/ /data/web_static/current/
+sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 
-sudo sed -i "/listen 80 default_server/a location /hbnb_static {alias /data/web_static/current/;}" /etc/nginx/sites-available/default
+sudo sed -i "/listen 80 default_server/a location /hbnb_static/ {alias /data/web_static/current/;}" /etc/nginx/sites-available/default
 sudo ufw allow "Nginx HTTP"
 sudo service nginx restart
 exit 0
