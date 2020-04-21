120aecf1ec8be989e9081258ba0f1eb418a26740
ACholberton <1103@holbertonschool.com>
2020-04-21 05:01:35 +0000

    updated file with the correct code name

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,11 +6,17 @@ script that starts a Flask web application
 from flask import Flask
 
 
-app = flask(__name__)
-@app.route('/')
+app = Flask(__name__)
+@app.route('/', strict_slashes=False)
 def hello():
     """runs application on host 0.0.0.0 port 5000"""
     return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    """runs application on host 0.0.0.0 port 5000"""
+    return 'HBNB'
 
 
 if __name__ == '__main__':
