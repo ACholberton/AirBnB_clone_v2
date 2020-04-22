cad5c09132b76aa4264316d5655893d117706694
ACholberton <1103@holbertonschool.com>
2020-04-22 01:49:05 +0000

    fixed typo and changed description

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -29,9 +29,10 @@ def C_text(text):
 
 
 @app.route('/python/', strict_slashes=False)
-@app.route('/python/(<text>)', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def Python_text(text='is cool'):
-    """runs application on host 0.0.0.0 port 5000  and returns python + text"""
+    """runs application on host 0.0.0.0 port 5000  and returns python is cool
+    if text is None otherwise return python + text"""
     if text is None:
         return 'Python is cool'
     else:
