66a8ad730109b9b538230b2561aae7fb55db706b
ACholberton <1103@holbertonschool.com>
2020-04-22 00:39:48 +0000

    Created file that returns c + a text

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application
+"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """runs application on host 0.0.0.0 port 5000"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_HBNB():
+    """runs application on host 0.0.0.0 port 5000 and returns HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_text():
+        """runs application on host 0.0.0.0 port 5000  and returns c + text"""
+        return 'C {}'.format(text.replace("_", " "))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
